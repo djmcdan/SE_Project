2db3302e6609472a401518a649792303e41c5725
djmcdan <darius.j.mcdaniel@gmail.com>
2015-12-05 15:49:52 -0500

    Cleaned up and added dataframe with multiple tables and some queries

--- Q ___c_SQL.R
+++ Q ___c_SQL.R
@@ -1,64 +1,73 @@
 #c. SQL (sqldf / Rsqlite)
-
+setwd("C:/Users/Darius/Documents/DF2015_Data/DF2015_revised") 
 
 #Create a SQLite database and figure out how many purchases where classified as Trucks, SUVs and cars , and the popular years for makers using different queries?
 
 
-
-install.packages('sqldf')
 library(sqldf)
+library(tcltk)
+require(RSQLite)
 
-#Lets see how many new cars where bought by year
 
-sqldf("select model_year_bought, count(*) from new_bought_latlon group by model_year_bought")
-
-#This shows us that there are 52 different body types
-
-sqldf("select bodytype_bought, count(*) as freq from new_bought_latlon group by bodytype_bought order by freq desc")
-sqldf("select bodytype_bought, count(*) as freq from use_bought_latlon group by bodytype_bought order by freq desc")
-
-#How many new and used purchases by makes
-
-sqldf("select make_bought, count(*) as freq from new_bought_latlon group by make_bought order by freq desc")
-
-sqldf("select make_bought, count(*) as freq from use_bought_latlon group by make_bought order by freq desc")
-
-#What was the average, min and max mileages bought along with the average, min and max price bought 
-#New
-mb_avs_nw <- sqldf("select make_bought, count(*) as freq, min(price_bought), avg(price_bought), max(price_bought),min(mileage_bought),  avg(mileage_bought), max(mileage_bought)  
-      from new_bought_latlon group by make_bought order by freq desc")
-#Used
-mb_avs_us <- sqldf("select make_bought, count(*) as freq, min(price_bought), avg(price_bought), max(price_bought),min(mileage_bought),  avg(mileage_bought), max(mileage_bought)  
-      from use_bought_latlon group by make_bought order by freq desc")
-mb_avs_us
-
-#What are the makes that had more than 3000 new purhases
-
-sqldf("select * from mb_avs_nw where freq > 3000 order by freq desc")
-
-#What are the makes that had more than 600 new purhases
-sqldf("select * from mb_avs_us where freq > 600 order by freq desc")
+db <- dbConnect(SQLite(), dbname="Edmonds.sqlite")
 
 
 
 
+
+#Below write the different dataset into sql tables
+
+dbWriteTable(conn = db, name = "Visitor", value = "visitor.csv",
+             row.names = FALSE, header = TRUE)
+dbWriteTable(conn = db, name = "Transactions", value = "transactions.csv",
+             row.names = FALSE, header = TRUE)
+dbWriteTable(conn = db, name = "Configuration", value = "configuration.csv",
+             row.names = FALSE, header = TRUE)
+dbWriteTable(conn = db, name = "Leads", value = "leads.csv",
+             row.names = FALSE, header = TRUE)
+dbWriteTable(conn = db, name = "Shopping", value = "shopping.csv",
+             row.names = FALSE, header = TRUE)
+
+
+# The tables in the database
+dbListTables(db)     
+
+# The columns in Visitor and Transactions
+dbListFields(db, "Visitor")         
+dbListFields(db, "Transactions")  
+
+
+      
+#Lets see how many new cars where bought by manufactuer year
+dbGetQuery(db,"SELECT model_year_bought as Model_year_purchased_New, count(*) as modelYR_freq from Transactions  WHERE new_or_used_bought = 'N' GROUP BY model_year_bought")
+
+dbGetQuery(db,"SELECT model_year_bought as Model_year_purchased_Used, count(*) as modelYR_freq from Transactions  WHERE new_or_used_bought = 'U' GROUP BY model_year_bought")
+
+
+#How many different body types were purchased new and used displaying only the top 10
+
+dbGetQuery(db,"select bodytype_bought, count(*) as freq from Transactions WHERE new_or_used_bought = 'N' group by bodytype_bought order by freq desc")
+dbGetQuery(db,"select bodytype_bought, count(*) as freq from Transactions WHERE new_or_used_bought = 'U' group by bodytype_bought order by freq desc")
+
+#What are the top five makers that sold the most new and used cars 
+
+dbGetQuery(db,"select make_bought, count(*) as freq from Transactions WHERE new_or_used_bought = 'N'  GROUP BY make_bought  order by freq desc LIMIT 5")
+
+dbGetQuery(db,"select make_bought, count(*) as freq from Transactions WHERE new_or_used_bought = 'U' GROUP BY make_bought order by freq desc LIMIT 5")
 
 #Honda and Toyota both have the most purchases for new and used
 
 
+#What was the average, min and max mileages bought along with the average, min and max price bought for the top ten
 
-#So we can see thather are outlier that have high mileage in the 20000s
-
-head(visitor,5)
-
-
-SELECT last_name, first_name, avg(salary) AS average_salary, 
-count(*) AS years_worked 
-FROM employees 
-GROUP BY last_name, first_name 
-HAVING count(*) > 2 
-ORDER BY avg(salary) DESC;
+#New
+dbGetQuery(db,"select make_bought, count(*) as freq, min(price_bought), avg(price_bought), max(price_bought),min(mileage_bought),  avg(mileage_bought), max(mileage_bought)  
+      from Transactions WHERE new_or_used_bought = 'N'  GROUP BY make_bought order by freq desc LIMIT 10")
+#Used
+dbGetQuery(db,"select make_bought, count(*) as freq, min(price_bought), avg(price_bought), max(price_bought),min(mileage_bought),  avg(mileage_bought), max(mileage_bought)  
+      from Transactions WHERE new_or_used_bought = 'U'  GROUP BY make_bought order by freq desc LIMIT 10")
 
 
 
+#Honda and Toyota both have the most purchases for new and used
 
