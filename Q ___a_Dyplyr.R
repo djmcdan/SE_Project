bf67ce4bcc9c18518f9c5959a9ac331c37af0b68
djmcdan <darius.j.mcdaniel@gmail.com>
2015-12-05 17:30:08 -0500

    Cleaned and ready

--- Q ___a_Dyplyr.R
+++ Q ___a_Dyplyr.R
@@ -10,14 +10,16 @@ source("SE_projectData.R")
 
 visitor_time <- visitor %>% 
   mutate(new_dwell_time_mins = new_dwell_time/60, used_dwell_time_mins = used_dwell_time/60 , tot_dwell_time_mins =   tot_dwell_time/60 , tot_dwell_time_hours = tot_dwell_time_mins/60)%>% 
-  select(visitor_key, new_dwell_time_mins,used_dwell_time_mins,tot_dwell_time_mins,tot_dwell_time_hours, zip)
-
-mean_visitor_time <- visitor_time %>% summarise(avgvisit_time = mean(tot_dwell_time_mins, na.rm = TRUE)) 
-
-med_visitor_time <- visitor_time %>% summarise(med_visit_time = median(tot_dwell_time_mins, na.rm = TRUE))
+  select(visitor_key, tot_dwell_time, tot_dwell_time_mins,tot_dwell_time_hours, zip)
 
 
-names(visitor_time)
+sum_visitor_time <- visitor_time %>% group_by(visitor_key)%>% summarise(hours_sum_time = sum(tot_dwell_time_hours, na.rm = TRUE))
+
+#What was the average,min and max time spent on the site for all visits
+
+mean_med_visitor_time <-sum_visitor_time %>% summarise(mean_hours_visited = mean(hours_sum_time , na.rm = TRUE),median_hours_visited = median(hours_sum_time , na.rm = TRUE))
+
+mean_med_visitor_time
 
 
 #Now for those that purchased new or used car what was the avg time visited?
@@ -27,31 +29,42 @@ used_bought <- transactions %>% filter(new_or_used_bought == "U") %>% select(vis
 
 
 
-visitor_purchased_new <- visitor_time %>%inner_join(new_bought)%>% distinct()
-visitor_purchased_used <- visitor_time %>%inner_join(used_bought)%>% distinct()
+visitor_purchased_new <- sum_visitor_time %>%inner_join(new_bought)%>% distinct()
+visitor_purchased_used <- sum_visitor_time %>%inner_join(used_bought)%>% distinct()
+
+avgs_visitor_time_purchaseNw <-visitor_purchased_new %>% summarise(min_hours_visited = min(hours_sum_time , na.rm = TRUE), mean_hours_visited = mean(hours_sum_time , na.rm = TRUE),med_hours_visited = median(hours_sum_time , na.rm = TRUE), max_hours_visited = max(hours_sum_time , na.rm = TRUE))
+avgs_visitor_time_purchaseUs <-visitor_purchased_used %>% summarise(min_hours_visited = min(hours_sum_time , na.rm = TRUE), mean_hours_visited = mean(hours_sum_time , na.rm = TRUE),med_hours_visited = median(hours_sum_time , na.rm = TRUE), max_hours_visited = max(hours_sum_time , na.rm = TRUE))
+
+avgs_visitor_time_purchaseNw
+avgs_visitor_time_purchaseUs
+
 
 
 #How many purchases were  >1hr, 1-5, 5-10,10-20, gt20 hrs
 
-new_bought_cat <- visitor_purchased_new %>% mutate(time_hours_cat = ifelse(tot_dwell_time_hours < 1, 1, ifelse(tot_dwell_time_hours >= 1 & tot_dwell_time_hours < 5,2,
-                                                                                                               ifelse(tot_dwell_time_hours >= 5 & tot_dwell_time_hours < 10,3,   
-                                                                                                                      ifelse(tot_dwell_time_hours >= 10 & tot_dwell_time_hours < 20,4,
-                                                                                                                             ifelse(tot_dwell_time_hours >= 20 & tot_dwell_time_hours < 48,5,
-                                                                                                                                    ifelse(tot_dwell_time_hours >= 48 & tot_dwell_time_hours < 72,6,
-                                                                                                                                           ifelse(tot_dwell_time_hours >72,7,NA)))))))) 
+new_bought_cat <- visitor_purchased_new %>% mutate(time_hours_cat = ifelse(hours_sum_time < 1, 1, ifelse(hours_sum_time >= 1 & hours_sum_time < 5,2,
+                                                                                                               ifelse(hours_sum_time >= 5 & hours_sum_time < 10,3,   
+                                                                                                                      ifelse(hours_sum_time >= 10 & hours_sum_time < 20,4,
+                                                                                                                             ifelse(hours_sum_time >= 20 & hours_sum_time < 48,5,
+                                                                                                                                    ifelse(hours_sum_time >= 48 & hours_sum_time < 72,6,
+                                                                                                                                           ifelse(hours_sum_time >72,7,NA)))))))) 
 new_bought_cat$time_hours_cat<- factor(new_bought_cat$time_hours_cat, levels = c(1,2,3,4,5,6,7), labels = c(">1hr", " 1-5hrs", " 5-10hrs","10-20hrs","20-48hrs", "48-72hrs", ">72hrs")) 
 
 
-used_bought_cat <- visitor_purchased_used %>% mutate(time_hours_cat = ifelse(tot_dwell_time_hours < 1, 1, ifelse(tot_dwell_time_hours >= 1 & tot_dwell_time_hours < 5,2,
-                                                                                                                 ifelse(tot_dwell_time_hours >= 5 & tot_dwell_time_hours < 10,3,   
-                                                                                                                        ifelse(tot_dwell_time_hours >= 10 & tot_dwell_time_hours < 20,4,
-                                                                                                                               ifelse(tot_dwell_time_hours >= 20 & tot_dwell_time_hours < 48,5,
-                                                                                                                                      ifelse(tot_dwell_time_hours >= 48 & tot_dwell_time_hours < 72,6,
-                                                                                                                                             ifelse(tot_dwell_time_hours >72,7,NA)))))))) 
+new_bought_cat$hours_sum_time_ <- round(new_bought_cat$hours_sum_time , 2)
+
+
+used_bought_cat <- visitor_purchased_used %>% mutate(time_hours_cat = ifelse(hours_sum_time < 1, 1, ifelse(hours_sum_time >= 1 & hours_sum_time < 5,2,
+                                                                                                                 ifelse(hours_sum_time >= 5 & hours_sum_time < 10,3,   
+                                                                                                                        ifelse(hours_sum_time >= 10 & hours_sum_time < 20,4,
+                                                                                                                               ifelse(hours_sum_time >= 20 & hours_sum_time < 48,5,
+                                                                                                                                      ifelse(hours_sum_time >= 48 & hours_sum_time < 72,6,
+                                                                                                                                             ifelse(hours_sum_time >72,7,NA)))))))) 
 
 
 used_bought_cat$time_hours_cat<- factor(used_bought_cat$time_hours_cat, levels = c(1,2,3,4,5,6,7), labels = c(">1hr", " 1-5hrs", " 5-10hrs","10-20hrs","20-48hrs", "48-72hrs", ">72hrs")) 
 
+used_bought_cat$hours_sum_time_ <- round(used_bought_cat$hours_sum_time , 2)
 
 table(new_bought_cat$time_hours_cat )
 table(used_bought_cat$time_hours_cat )
