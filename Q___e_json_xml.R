c1e25d5ce0d99aacf582a70b7ea713383d4eec8f
djmcdan <darius.j.mcdaniel@gmail.com>
2015-12-06 23:14:55 -0500

    Stuck on second part of json api. Taking a break

--- Q___e_json_xml.R
+++ Q___e_json_xml.R
@@ -25,38 +25,6 @@ dbGetQuery(db,"select make_bought, count(*) as freq,model_bought,model_year_boug
 
 #Edmonds API key  d2d4xfrkywhw6dmmh55qjuma
 
-#NEW top 5 purchased
-api<- paste("https://api.edmunds.com/api/vehiclereviews/v2/honda/accord/2015?fmt=json&api_key=akw86gbvscxm4szkb3t53rnw",sep="")
-api<- paste("https://api.edmunds.com/api/vehiclereviews/v2/honda/cr-v/2014?fmt=json&api_key=akw86gbvscxm4szkb3t53rnw",sep="")
-api<- paste("https://api.edmunds.com/api/vehiclereviews/v2/honda/civic/2015?fmt=json&api_key=akw86gbvscxm4szkb3t53rnw",sep="")
-api<- paste("https://api.edmunds.com/api/vehiclereviews/v2/honda/odyssey/2015?fmt=json&api_key=akw86gbvscxm4szkb3t53rnw",sep="")
-
-#Useds top 5 purchased
-api<- paste("https://api.edmunds.com/api/vehicle/v2/honda/accord/2014?fmt=json&api_key=akw86gbvscxm4szkb3t53rnw",sep="")
-
-
-addr <- getURL(api)
-
-url.json <- fromJSON(addr)
-
-str(url.json,1)
-str(url.json,2)
-str(url.json,3)
-str(url.json,4)
-str(url.json,5)
-
-
-HondaOdyssey2014Ratings <- unlist(url.json$averageRating[1]) 
-
-
-unlist(url.json$location[5]) -> STATE
-unlist(url.json$location[4]) -> COUNTRY
-
-HondaAccord2015Ratings <- unlist(url.json$averageRating[1]) 
-HondaCRV2014Ratings <- unlist(url.json$averageRating[1]) 
-HondaCivic2015Ratings <- unlist(url.json$averageRating[1]) 
-HondaOdyssey2015Ratings <- unlist(url.json$averageRating[1]) 
-
 
 #Create a function that gives consumer data ratings for cars pulling from Edmonds
 
@@ -68,8 +36,10 @@ cars <- function(id,make,model,year) {
   #Main part of api
   api_first<- paste("https://api.edmunds.com/api/vehiclereviews/v2/",sep="")
   
-  
-  make_mod_yr <- paste(make,model,year,sep = "/")
+  makeLow <-tolower(make)
+  modelLow <-tolower(model)
+  yearLow <-tolower(year)
+  make_mod_yr <- paste(makeLow,modelLow,yearLow,sep = "/")
 
 
   api_last <- paste("?fmt=json&api_key=akw86gbvscxm4szkb3t53rnw",sep="")
@@ -83,39 +53,90 @@ cars <- function(id,make,model,year) {
   url.json <- fromJSON(addr)
   
   #Used to count for correct parsing
-  #str(url.json,2)
+  str(url.json,2)
   
   Ratings <- unlist(url.json$averageRating[1])
   
 
   #This gives us all personal weather stations data in area specified by lat and lon
-  df <- rbind(id, make, model, year, Ratings )
-  df_ <- data.frame(df)
-  df_n <- t(df_)
-  df_n_ <- data.frame(df_n)
-  
-  
+  df_n_ = data.frame(id, makeLow, modelLow, yearLow, Ratings )
     #This returns url, location and station data for lat lon
   return(df_n_)
   
   
 }
 
-cars(id=1,make="honda",model= "odyssey", year = 2015) ->t1
-cars(id=2,make="honda",model= "odyssey", year = 2015) ->t2
-cars(id=3,make="honda",model= "odyssey", year = 2015) ->t3
+
+#NEW top 5 purchased
+
+
+cars(id=1,make="HONDA",model= "acCord", year = 2015) ->t1
+cars(id=2,make="honDa",model= "Cr-v", year = 2014) ->t2
+cars(id=3,make="honda",model= "civic", year = 2015) ->t3
 cars(id=4,make="honda",model= "odyssey", year = 2015) ->t4
-cars(id=5,make="honda",model= "odyssey", year = 2015) ->t4
+cars(id=5,make="Subaru",model= "forester", year = 2015) ->t5
 
 
-#QA
-
-myPWS(lat=-23.533333,lon=-46.616666,dist=3)   # Sao Paulo Brazil
-myPWS(lat=48.86666,lon=2.333333,dist=1)  # Paris, France
-myPWS(lat=42.3313889,lon=-83.045833,dist=5) # Detroit, Michigan
-myPWS(lat=40.714166,lon=-74.00638,dist=5) # New York, New York
+#Used top 5 purchased
 
 
 
 
+#This is nice but I want to just add this rating to my data sent of all makes and models
 
+carsdf <- function(df) {
+
+  
+  masterlist <- list() # Blank master list
+  jj = 1 # Counter for incrementing the masterlist
+  
+  
+  for (ii in 1:length(df)) {
+    
+  #Main part of api
+  api_first<- paste("https://api.edmunds.com/api/vehiclereviews/v2/",sep="")
+  
+  makeLow <-tolower(df$make)
+  modelLow <-tolower(df$model)
+  yearLow <-tolower(df$year)
+  
+  make_mod_yr <- paste(makeLow,modelLow,yearLow,sep = "/") 
+  
+  api_last <- paste("?fmt=json&api_key=akw86gbvscxm4szkb3t53rnw",sep="")
+  url <- paste(api_first,make_mod_yr,api_last,sep = "")
+  addr <- getURL(url)
+  
+  
+  
+  
+  #JSON parsed
+  url.json <- fromJSON(addr)
+  
+  #Used to count for correct parsing
+  #str(url.json,2)
+  
+  Ratings <- unlist(url.json$averageRating[1])
+  
+  
+  #This gives us all personal weather stations data in area specified by lat and lon
+#   df <- rbind(id, makeLow, modelLow, yearLow, Ratings )
+#   df_ <- data.frame(df)
+#   df_n <- t(df_)
+#   df_n_ <- data.frame(df_n)
+#   
+
+  
+    masterlist[[jj]] <- list(makeLow=makeLow,modelLow=modelLow,yearLow=yearLow,Ratings=Ratings)
+  jj = jj + 1
+  
+  
+}
+car_rating <-  do.call(rbind,masterlist)
+return(car_rating)
+}
+
+
+carsdf(New_data) ->c1
+
+
+New_data<- dbGetQuery(db,"select make_bought as make, count(*) as freq,model_bought as model,model_year_bought as year from Transactions WHERE new_or_used_bought = 'N'  GROUP BY make_bought,model_bought  order by freq desc LIMIT 5")
